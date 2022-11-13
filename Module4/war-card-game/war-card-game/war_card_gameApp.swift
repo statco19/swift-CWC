a4c28b58fd9976eb761cafb4731090ee3497ea95
andrew <statco19@gmail.com>
2022-11-13 20:00:04 +0900

    Add war card game from Module 1.

---
+++ Module4/war-card-game/war-card-game/war_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_gameApp.swift
+//  war-card-game
+//
+//  Created by Changjun Oh on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct war_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
