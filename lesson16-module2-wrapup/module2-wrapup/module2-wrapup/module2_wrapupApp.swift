270994397d2c7a962b1d717608237b7d7ee3d531
andrew <statco19@gmail.com>
2022-11-13 17:14:05 +0900

    lesson 16 module 2 wrap up challenge

---
+++ lesson16-module2-wrapup/module2-wrapup/module2-wrapup/module2_wrapupApp.swift
@@ -0,0 +1,17 @@
+//
+//  module2_wrapupApp.swift
+//  module2-wrapup
+//
+//  Created by Changjun Oh on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct module2_wrapupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
