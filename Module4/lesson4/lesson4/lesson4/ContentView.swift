4a5b9882978f2f9ec98b8247736e552c27672273
andrew <statco19@gmail.com>
2022-11-14 12:24:59 +0900

    Create module 4 : lesson 4

---
+++ Module4/lesson4/lesson4/lesson4/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  lesson4
+//
+//  Created by Changjun Oh on 2022/11/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            GeometryReader { geo in
+                Rectangle()
+                    .frame(width: geo.size.width/4, height: geo.size.height, alignment: .center)
+                    .onTapGesture {
+                        print("global x: \(geo.frame(in: .global).minX), y: \(geo.frame(in: .global).minY)")
+                        print("local x: \(geo.frame(in: .local).minX), y: \(geo.frame(in: .local).minY)")
+                    }
+            }
+//            .padding([.top, .leading], 30)
+//            .offset(x:30, y:30)
+            .position(x:400, y:300)
+            
+            GeometryReader { geo in
+                Rectangle()
+                    .foregroundColor(.green)
+                    .frame(width: geo.size.width/4, height: geo.size.height, alignment: .center)
+                    .onTapGesture {
+                        print("global x: \(geo.frame(in: .global).minX), y: \(geo.frame(in: .global).minY)")
+                        print("local x: \(geo.frame(in: .local).minX), y: \(geo.frame(in: .local).minY)")
+                    }
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Module4/lesson4/lesson4/lesson4/lesson4App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson4App.swift
+//  lesson4
+//
+//  Created by Changjun Oh on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct lesson4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
